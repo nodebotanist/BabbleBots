aeb0723d0889d27fb042e3ccf460d0dd548ba6b1
Kassandra Perch <kassandra@kassandraperch.com>
2015-10-16 20:58:29 -0500

    setPin works for digital. Requires testing at home for PWM

--- examples/setPin-client/setPin-client.ino
+++ examples/setPin-client/setPin-client.ino
@@ -14,11 +14,28 @@ void loop(){
 }
 
 int setPin(String cmd){
-    char* myCopy = strtok(strdup(cmd), "|");
-    String pin = myCopy;
-    myCopy = strtok(NULL, ","); 
-    int value= atoi(myCopy);
+    //convert ascii to integer
+    int pinNumber = cmd.charAt(1) - '0';
+    //Sanity check to see if the pin numbers are within limits
+    if (pinNumber< 0 || pinNumber >7) return -1;
 
+    else if (cmd.startsWith("A"))
+    {
+        pinNumber = pinNumber + 10;
+    }
+    
+    char* myCopy = strtok(strdup(cmd), "|");
+    myCopy = strtok(NULL, "|"); 
+    int value= atoi(myCopy);
+    
+    if(value == 0 || value == 1){
+        pinMode(pinNumber, OUTPUT);
+        digitalWrite(pinNumber, value);
+    } else {
+        pinMode(pinNumber, OUTPUT);
+        analogWrite(pinNumber, value);
+    }
     return 1;
+    
 }
 
